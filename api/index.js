679809296ff0dfaf2f6c33027aa198ddf250713e
jinsean88 <jinsean88@gmail.com>
2024-05-27 17:46:49 +0800

    Change Directory Order

---
+++ api/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js"; //Add ".js" if import from express server
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+
+
+
+dotenv.config();
+const app = express();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser())
+
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.errorStatus || 500;
+  const errorMessage = err.message || "Something Went Wrong!" ;
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(8080, () => {
+  connect()
+  console.log("Connected to backend.")
+  });
+
+
