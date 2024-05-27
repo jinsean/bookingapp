0b20c223d1ea59e7c9310bf27ef00d80dd611a91
jinsean88 <jinsean88@gmail.com>
2024-05-27 09:17:58 +0800

    Added verfiyadmin to hotel

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -16,7 +16,6 @@ export const register = async (req, res, next) => {
 
         await newUser.save();
         res.status(200).send("User has been created");  
-    
     } catch (err) {
         next(err);
     }
@@ -40,14 +39,14 @@ export const login = async (req, res, next) => {
             { id: user._id, isAdmin: user.isAdmin },
             process.env.JWT
         );
-
+        //To hide password, isadmin in consolelog
         const { password, isAdmin, ...otherDetails } = user._doc;
         res
           .cookie("access_token", token, {
             httpOnly: true,
           })
           .status(200)
-          .json({ ...otherDetails });
+          .json({ details: { ...otherDetails }, isAdmin });
     } catch (error) {
         next(err);
     }
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,17 +1,18 @@
 import express from "express";
-import Hotel from "../models/Hotel.js"
 import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import {verifyAdmin} from "../utils/verifyToken.js"
+
 
 const router = express.Router();  
 
-//Post  
-router.post("/", createHotel);
+//Create  
+router.post("/", verifyAdmin, createHotel);
 
 //Update  
-router.put("/:id", updateHotel);
+router.put("/:id", verifyAdmin, updateHotel);
 
 //Delete
-router.delete("/:id", deleteHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
 
 //Get by ID
 router.get("/:id", getHotel);
